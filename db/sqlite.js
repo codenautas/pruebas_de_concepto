8d30b5493e7d6e0f83b32f966cd9b1e80fabcc0c
Diego F.(i7) <diegoefe@gmail.com>
2015-09-15 08:59:57 -0300

    Modifico test sqlite para probar con db en archivo

--- db/sqlite.js
+++ db/sqlite.js
@@ -1,14 +1,19 @@
 "use strict";
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+//var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('PEPE.db', sqlite3.OPEN_CREATE |sqlite3.OPEN_READWRITE);
+
+console.log("db", db);
 
 db.serialize(function() {
+  db.run("DROP TABLE IF EXISTS lorem");
   db.run("CREATE TABLE lorem (info TEXT)");
     
   db.run("INSERT INTO lorem VALUES('simple run')");
 
   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  console.log("stmt", stmt);
   for (var i = 0; i < 10; i++) {
       stmt.run("Ipsum " + i);
   }
