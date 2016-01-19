4f0d89790db3976b839f4e0476435c4df62bb188
Diego F.(i7) <diegoefe@gmail.com>
2016-01-19 11:05:42 -0300

    Prueba de htmlparser2

--- htmlparsers/pru-htmlparser2.js
+++ htmlparsers/pru-htmlparser2.js
@@ -1,7 +1,30 @@
 var fs = require("fs-promise");
 var htmlparser = require("htmlparser2");
 
-fs.readFile('./pag1.htm', {enconding:'utf8'}).catch(function(err) {
+fs.readFile('./pag0.htm', {enconding:'utf8'}).catch(function(err) {
     console.log("error", err);
 }).then(function(content) {
+    var parser = new htmlparser.Parser({
+        onopentag: function(name, attribs){
+            console.log("open", name);
+            if(name === "script" && attribs.type === "text/javascript"){
+                console.log("JS! Hooray!");
+            }
+        },
+        ontext: function(text){
+            console.log("-->", text);
+        },
+        onclosetag: function(tagname){
+            console.log("close", tagname);
+            if(tagname === "script"){
+                console.log("That's it?!");
+            }
+        }
+    }, {decodeEntities: true});
+    parser.write(content);
+    parser.end();
+}).catch(function(err) {
+    console.log("error", err);
 });
+
+
