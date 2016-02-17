67389e4bd0239390a8bcb6f870b214bf55e5b7e0
Diego F.(i7) <diegoefe@gmail.com>
2016-02-17 08:46:12 -0300

    read from stdion, new form

--- readstdin/rstdin1.js
+++ readstdin/rstdin1.js
@@ -3,6 +3,7 @@ process.stdin.resume();
   var util = require('util');
 
   process.stdin.on('data', function (text) {
+      console.log("data", text);
     console.log('received data:', util.inspect(text));
     //if (text.match(/quit\r?\n/)) {
     if (text.match(/\r?\n/)) {
