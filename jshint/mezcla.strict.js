3097bc989fe4c0e8a4d2bc11a313dc3cbb088f86
Emilio <Platzer>
2016-03-28 15:00:39 -0300

    probando jshint

---
+++ jshint/mezcla.strict.js
@@ -0,0 +1,10 @@
+/*jshint E059:false */
+"use strict";
+/*jshint eqnull:true */
+/*jshint globalstrict:true */
+/*jshint node:true */
+/*eslint-disable no-console */
+
+var nombreDelModulo = {};
+
+console.log(nombreDelModulo)
