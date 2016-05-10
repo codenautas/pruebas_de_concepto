cb6e73962888f025e4238b6e3292a89aff2475ee
Emilio <Platzer>
2016-05-10 11:29:56 -0300

    Prueba exitosa con tweets.

---
+++ twitter/trac.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var localConfig = require('./local-config.json');
+
+console.log('localConfig', localConfig);
+
+var Twitter = require('node-tweet-stream')
+  , t = new Twitter({
+    consumer_key:    localConfig.consumer_key,
+    consumer_secret: localConfig.consumer_secret,
+    token:           localConfig.token,
+    token_secret:    localConfig.token_secret
+  })
+ 
+t.on('tweet', function (tweet) {
+  console.log('tweet received', tweet.text, tweet.created_at, tweet.user.name);
+})
+ 
+t.on('error', function (err) {
+  console.log('Oh no', err)
+})
+ 
+t.track('nodejs')
+t.track('postgresql')
+ 
+// 5 minutes later
+t.track('tacos')
+ 
+// 10 minutes later
+t.untrack('pizza')
+
+setInterval(function(){
+    console.log('stamp',new Date());
+},5000);
