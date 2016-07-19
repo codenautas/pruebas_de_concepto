7c990e4601a6d9dad876abb0cc0d2abc4be61959
Diego F.(EW7) <diegoefe@gmail.com>
2016-07-19 13:49:58 -0300

    PopUp: agrego js-to-html

--- popups/popup.js
+++ popups/popup.js
@@ -1,25 +1,28 @@
 "use strict";
 
 function PopUp(divName) {
-    this.div = divName;
-    var wok = window.onclick;
+    this.divName = divName;
     var me = this;
-    function windowOnClick(event) {
-        if (event.target == me.w()) {
-            me.hide();
-        }
-    };
+    //var divID = divName;
+    var divID = 'pepe';
+    this.div = document.getElementById(divID);
+    if(this.div) { throw new Error('PopUp() needs to create div "'+divName+'"'); }
+    var html=jsToHtml.html;
+    this.div = html.div({id:divID}).create();
+    var body = document.body;
+    body.insertBefore(this.div, body.firstChild);
+    // cerrar ventana al clickear afuera
+    function windowOnClick(event) { if(event.target == me.w()) { me.hide(); } };
+    var wok = window.onclick;
     if(wok) {
         window.onclick = function(event) {
             wok(event);
             windowOnClick(event);
         }
-    } else {
-        window.onclick = windowOnClick;
-    }
+    } else { window.onclick = windowOnClick; }
 }
 PopUp.prototype.w = function w() {
-    return document.getElementById(this.div);
+    return document.getElementById(this.divName);
 }
 PopUp.prototype.show = function show() {
     this.w().style.display='block';
