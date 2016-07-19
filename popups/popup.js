8aa2a50173392b086aae7a992273def67741ce37
Diego F.(EW7) <diegoefe@gmail.com>
2016-07-19 15:34:43 -0300

    PopUp: limpio html

--- popups/popup.js
+++ popups/popup.js
@@ -1,22 +1,22 @@
 "use strict";
 
-function PopUp(divName) {
-    this.divName = divName;
+function PopUp(opts) {
+    var ops = opts || {};
+    var divName = ops.divName || 'pu_div';
     var me = this;
-    var divID = divName;
-    //var divID = 'pepe';
-    this.div = document.getElementById(divID);
+    this.div = document.getElementById(divName);
     if(this.div) { throw new Error('PopUp() needs to create div "'+divName+'"'); }
     
     var html=jsToHtml.html;
     var btnCancel = html.input({id:'pu_cancel', type:'button', class:'cancelbtn', value:'Cancel'}).create();
     var btnOK = html.input({id:'pu_ok', type:'button', class:'okbtn', value:'OK'});
     var margen = 'margen';
-    var container = html.div({class:'container'}, [
+    var container = html.div({id:'container', class:'container'}, [
         html.span({class:margen}, btnCancel),
         html.span({class:margen}, btnOK)
     ]).create();
-    this.div = html.div({id:divID, class:'modal'}).create();
+    
+    this.div = html.div({id:divName, class:'modal'}).create();
     this.div.appendChild(container);
     
     this.show = function() { me.div.style.display='block'; }
@@ -25,10 +25,10 @@ function PopUp(divName) {
     btnCancel.onclick = this.hide;
     
     var body = document.body;
-    //body.insertBefore(this.div, body.firstChild);
-    body.insertBefore(this.div, body.lastChild);
+    body.appendChild(this.div);
+    /*
     // cerrar ventana al clickear afuera
-    function windowOnClick(event) { if(event.target == me.div) { me.hide(); } };
+    function windowOnClick(event) { if(event.target==me.div) { me.hide(); } };
     var wok = window.onclick;
     if(wok) {
         window.onclick = function(event) {
@@ -36,4 +36,5 @@ function PopUp(divName) {
             windowOnClick(event);
         }
     } else { window.onclick = windowOnClick; }
+    */
 }
