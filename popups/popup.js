e7868accdacf31971ac55e387316b2ce53be4590
Diego F.(EW7) <diegoefe@gmail.com>
2016-07-19 17:23:33 -0300

    PopUp: trato de hacer demo para https://github.com/codenautas/backend-plus/issues/13 (sin éxito)

--- popups/popup.js
+++ popups/popup.js
@@ -1,30 +1,42 @@
 "use strict";
 
-function PopUp(opts) {
-    var ops = opts || {};
-    var divName = ops.divName || 'pu_div';
+function PopUp(cb) {
+    var divName = 'pu_div';
     var me = this;
+    this.cb = cb;
+    
     this.div = document.getElementById(divName);
-    if(this.div) { throw new Error('PopUp() needs to create div "'+divName+'"'); }
+    var body = document.body;
+    if(this.div) {
+        body.removeChild(this.div);
+        //throw new Error('PopUp() needs to create div "'+divName+'"'); 
+    }
     
     var html=jsToHtml.html;
-    var btnCancel = html.input({id:'pu_cancel', type:'button', class:'cancelbtn', value:'Cancel'}).create();
-    var btnOK = html.input({id:'pu_ok', type:'button', class:'okbtn', value:'OK'});
+    //var btnCancel = html.input({id:'pu_cancel', type:'button', class:'cancelbtn', value:'X'}).create();
+    //var btnOK = html.input({id:'pu_ok', type:'button', class:'okbtn', value:'OK'});
     var margen = 'margen';
+    var cruz = html.span({class:'close', title:'Cerrar'}, "x").create();
+    var imgContainer = html.div({class:'imgcontainer'}, [cruz]);
+    var innerContainer = html.div({class:'cancelbtn'}).create();
     var container = html.div({id:'container', class:'container'}, [
-        html.span({class:margen}, btnCancel),
-        html.span({class:margen}, btnOK)
+        //html.span({class:margen}, btnCancel),
+        innerContainer,
+        //html.span({class:margen}, btnOK)
     ]).create();
     
-    var outDiv = html.div({class:['modalcontent','animate']}, [container]).create();
+    var outDiv = html.div({class:['modalcontent','animate']}, [imgContainer, container]).create();
     this.div = html.div({id:divName, class:'modal'}).create();
     this.div.appendChild(outDiv);
     
-    this.show = function() { me.div.style.display='block'; }
+    this.show = function() {
+        me.div.style.display='block';
+        return Promise.resolve(me.cb(innerContainer, me.hide));
+    }
     this.hide = function() { me.div.style.display='none'; }
 
-    btnCancel.addEventListener('click', this.hide);
-    var body = document.body;
+    // btnCancel.addEventListener('click', this.hide);
+    cruz.addEventListener('click', this.hide);
     body.appendChild(this.div);
     /*
     // cerrar ventana al clickear afuera (no funciona)
