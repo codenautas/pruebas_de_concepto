c1217708e513474dea2ab9bdcd5cefe5ccbf9024
Emilio <Platzer>
2016-07-20 10:18:23 -0300

    Empiezo probar de pasar a promesa el popup de Diego

---
+++ popups/dialog-promise.js
@@ -0,0 +1,39 @@
+"use strict";
+
+function showDialog(dialogConstructor){
+    return new Promise(function(resolve, reject) {
+        var divName = 'pu_div';
+        var dialogWindow = document.getElementById(divName);
+        var body = document.body;
+        if(dialogWindow) {
+            body.removeChild(dialogWindow);
+        }
+        var html=jsToHtml.html;
+        var margen = 'margen';
+        var cruz = html.span({class:'close', title:'Cerrar'}, "x").create();
+        var imgContainer = html.div({class:'imgcontainer'}, [cruz]);
+        var innerContainer = html.div({class:'cancelbtn'}).create();
+        var container = html.div({id:'container', class:'container'}, [
+            innerContainer,
+        ]).create();
+        var outDiv = html.div({class:['modalcontent','animate']}, [imgContainer, container]).create();
+        dialogWindow = html.div({id:divName, class:'modal'}).create();
+        dialogWindow.appendChild(outDiv);
+        // show:
+        dialogWindow.style.display='block';
+        // hide:
+        var hide = function() { 
+            dialogWindow.style.display='none'; 
+        }
+        var doneWithoutAnswer = function doneWithoutAnswer(message){
+            hide();
+            reject(new Error(message));
+        }
+        cruz.addEventListener('click', doneWithoutAnswer.bind(null,'closed by the \u00d7'));
+        body.appendChild(dialogWindow);
+        dialogConstructor(dialogWindow, function doneWithGoodAnswer(answer){
+            hide();
+            resolve(answer)
+        });
+    });
+}
