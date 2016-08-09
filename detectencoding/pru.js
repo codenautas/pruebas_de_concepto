256656bfa38f0de02e26747aa31b0a9cd4784b13
Diego F.(EW7) <diegoefe@gmail.com>
2016-08-09 17:06:43 -0300

    Corrijo test de detección y agrego is-utf8

--- detectencoding/pru.js
+++ detectencoding/pru.js
@@ -6,7 +6,8 @@ var fs = require('fs-promise');
 var Path = require('path');
 
 var jschardet = require("jschardet");
-var detect = require('charset-detector');
+var charsetDetector = require('charset-detector');
+var isUtf8 = require('is-utf8');
 
 var skipped = (process.env.SKIP || "").split(',');
 //console.log("skipped", skipped)
@@ -18,7 +19,7 @@ function setupFiles() {
     return fs.readdir(filesDir).then(function(files) {
         return Promises.all(files.map(function(file){
             var iFile = Path.join(filesDir, file);
-            return fs.readFile(iFile, 'binary').then(function(content){
+            return fs.readFile(iFile).then(function(content){
                 encFiles.push({
                     file: file,
                     content: content
@@ -39,7 +40,7 @@ describe('detect encoding', function(){
                     if(detected=='iso-8859-2') {
                         console.log("expected failure on '"+file.file+"'", detected, enc)
                     } else {
-                        expect(detected).to.eql(enc);                    
+                        expect(detected).to.eql(enc);
                     }
                 });
                 done();
@@ -54,16 +55,35 @@ describe('detect encoding', function(){
         it(testName, function(done){
             setupFiles().then(function() {
                 encFiles.forEach(function(file) {
-                    var detected = detect(file.content);
+                    var detected = charsetDetector(file.content)[0].charsetName.toLowerCase();
                     var enc = file.file.split('_')[0];
-                    console.log("detected", detected, "enc", enc, file.content)
-                    /*
-                    if(detected=='iso-8859-2') {
-                        console.log("expected failure on '"+file.file+"'", detected, enc)
+                    //console.log("detected", detected, "enc", enc, file.file)
+                    if(file.file.match(/con-bom/)) {
+                        expect(detected).to.eql('utf-8');
                     } else {
-                        expect(detected).to.eql(enc);                    
+                        expect(detected).to.eql('iso-8859-1');
                     }
-                    */
+                });
+                done();
+            }).catch(function(err) {
+                console.log("mal", err);
+                done(err);
+            });
+        });   
+    } else it.skip(testName);
+    testName = 'isUtf8';
+    if(skipped.indexOf(testName) == -1) {
+        it(testName, function(done){
+            setupFiles().then(function() {
+                encFiles.forEach(function(file) {
+                    var detected = isUtf8(file.content);
+                    var enc = file.file.split('_')[0];
+                    //console.log("detected", detected, "enc", enc)
+                    if(enc.match(/utf-8/)) {
+                        expect(detected).to.be.ok();
+                    } else {
+                        expect(detected).not.to.be.ok();
+                    }
                 });
                 done();
             }).catch(function(err) {
