9b27dcd7d86a37aa05405f1c8fb64eb96972d0f5
Emilio <emilioplatzer@gmail.com>
2016-09-14 15:49:58 -0300

    Pruebo la forma de detectar errores al considerar UTF8

--- char-converters/contar-palabras.js
+++ char-converters/contar-palabras.js
@@ -2,20 +2,34 @@ var iconv = require('iconv-lite');
 var fs = require('fs-promise');
 
 var fixtures=[
-    {nombre:'ansi'         , encoded: 'win1251'},
+    {nombre:'ansi'         , encoded: 'win1252'},
     {nombre:'ansi-win1250' , encoded: 'win1250'},
     {nombre:'ascii7'       ,                   },
     {nombre:'utf8'         ,                   },
 ];
 
 function contarPalabras(str){
-    return str.split(/[],;.!¡?¿: '"\n\r\t%&^[(){}]/).length
+    return str.split(/[,;.!¡?¿: '"\n\r\t%&^[\](){}]+/g).filter(function(word){ return word; }).length
 }
 
 Promise.all(
     fixtures.map(function(fixture){
         return fs.readFile('in-'+fixture.nombre+'.txt').then(function(buffer){
             if(fixture.encoded){
+                try{
+                    var imposible = buffer.toString();
+                    var bufferConv = new Buffer(imposible, 'utf8');
+                    if(imposible.substr(1).indexOf('\uFFFD')<0){
+                        console.log('Error. Se esperaba detectar un problema al considerar UTF8 a '+fixture.nombre+' que esta en '+fixture.encoded,imposible.substr(1).indexOf('\uFFFD'));
+                    }
+                    if(buffer==bufferConv){
+                        console.log('Error. Se esperaba detectar un problema al considerar UTF8 a '+fixture.nombre+' que esta en '+fixture.encoded,'son iguales');
+                    }
+                    fs.writeFile('local-in-'+fixture.nombre+'.txt', imposible);
+                }catch(err){
+                    console.log('Error ok',err);
+                    console.log(err.stack);
+                }
                 return iconv.decode(buffer, fixture.encoded);
             }
             return buffer.toString();
@@ -23,7 +37,7 @@ Promise.all(
             return fs.writeFile('local-res-step1-'+fixture.nombre+'.txt', str).then(function(){return str;});
         }).then(function(str){
             var count=contarPalabras(str);
-            var outstr=str+'Palabras: '+count+'\r\n';
+            var outstr=str+'\r\nPalabras: '+count+'\r\n';
             return fs.writeFile('local-res-step2-'+fixture.nombre+'.txt', outstr).then(function(){return outstr;});
         }).then(function(outstr){
             if(fixture.encoded){
@@ -35,10 +49,34 @@ Promise.all(
         }).catch(function(err){
             console.log('Error inesperado al procesar '+fixture.nombre);
             console.log(err);
+            throw err;
         });
     })
 ).then(function(){
-    
+    return fs.readdir('.');
+}).then(function(names){
+    return Promise.all(
+        names.filter(function(name){
+            return name.startsWith('local-res-');
+        }).map(function(name){
+            return fs.readFile(name).then(function(obtainedBuffer){
+                return fs.readFile(name.substr('local-res-'.length)).then(function(expectedBuffer){
+                    for(var i=0; i<Math.max(obtainedBuffer.length,expectedBuffer.length); i++){
+                        if(obtainedBuffer[i]!=expectedBuffer[i]){
+                            return 'Dif '+name+' pos '+i+' '+obtainedBuffer[i]+' != '+expectedBuffer[i];
+                        }
+                    }
+                    return null;
+                });
+            });
+        })
+    );
+}).then(function(results){
+    console.log('Fin');
+    console.log(results.filter(function(result){ return result; }));
+}).catch(function(err){
+    console.log('Error inesperado');
+    console.log(err);
 });
 
 // Convert from js string to an encoded buffer. 
