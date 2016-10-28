34b36ceb952cee4dc3cd1b5976cdf51e79f26513
Diego F.(EW7) <diegoefe@gmail.com>
2016-10-28 15:19:26 -0300

    Muestro info de .xls* leidos

--- xlsx/test.js
+++ xlsx/test.js
@@ -1,15 +1,22 @@
 "use strict"
 
 var XLSX = require('xlsx');
+var Promises = require('best-promise');
+var fs = require('fs-promise');
 
-var workbook = XLSX.readFile('ejemplo-2003.xls');
-
-var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) { /* iterate through sheets */
-  var worksheet = workbook.Sheets[y];
-  for (var z in worksheet) {
-    /* all keys that do not begin with "!" correspond to cell addresses */
-    if(z[0] === '!') continue;
-    console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
-  }
+fs.readdir('.').then(function(files) {
+    return Promises.all(files.filter(function(file) { return file.match(/(\.xlsx?)$/); }).map(function(xls) {
+        console.log('xls: ', xls);
+        var workbook = XLSX.readFile(xls);
+        var sheet_name_list = workbook.SheetNames;
+        sheet_name_list.forEach(function(y) { // iterate through sheets
+          var worksheet = workbook.Sheets[y];
+          for (var z in worksheet) {
+              console.log(' ', worksheet[z]);
+            // all keys that do not begin with "!" correspond to cell addresses
+            //if(z[0] === '!') continue;
+            //console.log('  ', y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+          }
+        });
+    }));
 });
