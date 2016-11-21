1fd50ae1201d87ddf7f661cf9af7d0f643fd3abe
Diego F.(EW7) <diegoefe@gmail.com>
2016-11-21 09:19:56 -0300

    Prueba con node-fetch

---
+++ node-fetch/pru.js
@@ -0,0 +1,28 @@
+var fetch = require('node-fetch');
+
+// if you are on node v0.10, set a Promise library first, eg.
+// fetch.Promise = require('bluebird');
+
+// plain text or html
+/*
+fetch('https://github.com/')
+    .then(function(res) {
+        return res.text();
+    }).then(function(body) {
+        console.log(body);
+    });
+*/
+// json
+
+var headers;
+fetch('https://api.github.com/users/github', {
+    headers: { 'User-Agent': 'Node-Fetch agent' },
+    })
+    .then(function(res) {
+        headers = res.headers.raw();
+        console.log("headers", headers);
+        return res.json();
+    }).then(function(json) {
+        console.log(json);
+        console.log("New full request can be done at: ", new Date(headers['x-ratelimit-reset'] * 1000))
+    });
