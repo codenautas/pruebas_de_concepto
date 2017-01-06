1f145a56555bb9e665563e2dea6dc4a61d4ebcbb
Emilio <emilioplatzer@gmail.com>
2017-01-06 16:34:24 -0300

    excel

--- excel-varios/test_xlsx.js
+++ excel-varios/test_xlsx.js
@@ -6,13 +6,14 @@ var fs = require('fs-promise');
 
 fs.readdir('.').then(function(files) {
     return Promises.all(files.filter(function(file) { return file.match(/(\.xlsx?)$/); }).map(function(xls) {
-        console.log('xls: ', xls);
+        console.log('----xls: ', xls);
         var workbook = XLSX.readFile(xls);
         var sheet_name_list = workbook.SheetNames;
         sheet_name_list.forEach(function(y) { // iterate through sheets
           var worksheet = workbook.Sheets[y];
+          console.log('----sheet',y);
           for(var z in worksheet) {
-              console.log(' ', worksheet[z]);
+              console.log('',z, worksheet[z]);
             // all keys that do not begin with "!" correspond to cell addresses
             //if(z[0] === '!') continue;
             //console.log('  ', y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
