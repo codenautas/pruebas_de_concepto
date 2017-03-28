a06892f2cb51e693c4e8e077c92d11d34aee44a3
Emilio <emilioplatzer@gmail.com>
2017-03-28 11:32:43 -0300

    prueba con interval

--- pg/prueba-pg.js
+++ pg/prueba-pg.js
@@ -1,6 +1,8 @@
 "use strict";
 
+
 var pg=require("pg");
+var PostgresInterval = require('postgres-interval');
 
 var auditCopy=require('audit-copy');
 
@@ -11,6 +13,27 @@ function logErr(err){
     console.log(err)
     console.log(err.stack)
 }
+
+var hola = function Saludo(texto){
+    this.texto=texto;
+}
+
+console.log('******************');
+console.log(new hola('hi'));
+
+var y = new PostgresInterval("4:12:00");
+var x = new PostgresInterval();
+
+console.log('-----------------');
+console.log('is ours');
+console.log(y);
+console.log(y.toPostgres());
+x.hours=4;
+x.minutes=12;
+console.log(x);
+console.log(x.toPostgres());
+
+process.exit();
 
 MiniTools.readConfig([
     {db:{idleTimeoutMillis:2000}},
@@ -26,19 +49,32 @@ MiniTools.readConfig([
 
 var pool1;
 
+
 function testTimeOut(config){
     var pool = new pg.Pool(config.db);
     console.log('SIN CONEXIONES');
     return pool.connect().then(function(client){
         console.log('1 CONEXION');
         console.log('pool', pool.pool._inUseObjects.length, pool.pool._inUseObjects[0]);
-        client.release();
-        console.log('pool release', pool.pool._inUseObjects.length, pool.pool.borrowed);
-        [1000,2000,3000].forEach(function(time){
-            sleep(time).then(function(){
-                console.log('FINALLY CONEXIONES');
-                console.log('pool',time,'sleep', pool.pool._inUseObjects.length, pool.pool.borrowed);
-            }).catch(logErr);
+        client.query("SELECT '1 year 11 days 13:45:15.12'::interval el_intervalo", function(err, result) {
+            console.log(result.rows[0]);
+            console.dir(result.rows[0].el_intervalo);
+            console.log(result.rows[0].el_intervalo);
+            console.log(JSON.stringify(result.rows[0].el_intervalo));
+            console.log(''+result.rows[0].el_intervalo);
+            console.log(result.rows[0].el_intervalo.toString());
+            console.log('=========');
+            for(var attr in result.rows[0].el_intervalo){
+                console.log(attr, result.rows[0].el_intervalo[attr]);
+            }
+            client.release();
+            console.log('pool release', pool.pool._inUseObjects.length, pool.pool.borrowed);
+            [1000,2000,3000].forEach(function(time){
+                sleep(time).then(function(){
+                    console.log('FINALLY CONEXIONES');
+                    console.log('pool',time,'sleep', pool.pool._inUseObjects.length, pool.pool.borrowed);
+                }).catch(logErr);
+            });
         });
     });
 }
