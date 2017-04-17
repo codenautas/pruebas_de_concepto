de9bf0816b67ec7a55dac720a630bad3574ec39a
Emilio <emilioplatzer@gmail.com>
2017-04-16 22:30:28 -0300

    auto-close

--- auto-close/prueba.js
+++ auto-close/prueba.js
@@ -1,5 +1,28 @@
 "use strict";
 
+var miniTools = require('mini-tools');
+var bestGlobals = require('best-globals');
+
 function cerrara(f){
     console.log('abriendo')
+    f().then(function(){
+        console.log('close');
+    },function(err){
+        console.log('close with err',err);
+    });
 }
+
+function unaParte(){
+    var q=Promise.resolve();
+    var p = bestGlobals.sleep(100).then(function(){
+        console.log('primera parada');
+        q=q.then(function(){
+            console.log('segunda parada');
+        });
+    }).then(function(){
+        return q;
+    });
+    return p;
+}
+
+cerrara(unaParte);
