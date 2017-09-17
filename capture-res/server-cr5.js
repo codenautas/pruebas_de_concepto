9a9b8f3df518c71e0ea6cca2d7c78ba89ddbc7ad
Emilio <emilioplatzer@gmail.com>
2017-09-16 21:16:32 -0300

    Ejemplo para stackoverflow

--- capture-res/server-cr5.js
+++ capture-res/server-cr5.js
@@ -1,31 +1,31 @@
-"use strict";
+    "use strict";
 
-var express = require('express')
-var app = express()
-var fs = require('fs');
+    var express = require('express')
+    var app = express()
+    var fs = require('fs');
 
-app.use(function(req,res,next){
-    res.originales={_writeRaw:res._writeRaw};
-    console.log('res.originales',res._send);
-    res._writeRaw = function _writeRaw(data, encoding, cb){
-        fs.appendFileSync('res.log',data);
-        return res.originales._writeRaw.apply(res, Array.prototype.slice.call(arguments));
-    };
-    next();
-});
+    app.use(function(req,res,next){
+        res.originales={_writeRaw:res._writeRaw};
+        console.log('res.originales',res._send);
+        res._writeRaw = function _writeRaw(data, encoding, cb){
+            fs.appendFileSync('res.log',data);
+            return res.originales._writeRaw.apply(res, Array.prototype.slice.call(arguments));
+        };
+        next();
+    });
 
-app.get('/hola2.txt', function (req, res) {
-    res.send('<b>Hola</b> Mundo!')
-})
+    app.get('/hola.txt', function (req, res) {
+        res.send('<b>Hola</b> Mundo textual!')
+    })
 
-app.get('/hola2.html', function (req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<b>Hola</b> Mundo!');
-    res.end();
-})
+    app.get('/hola.html', function (req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<b>Hola</b> Mundo hipervinculado!');
+        res.end();
+    })
 
-app.use(express.static('public'))
+    app.use(express.static('public'))
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+    })
