53649e7ff9b34acdf5c143dc0248352e168a8904
Emilio <emilioplatzer@gmail.com>
2018-01-18 10:49:39 -0300

    ejemplo con Promise.all

--- async/promesas-async.js
+++ async/promesas-async.js
@@ -2,6 +2,8 @@
 
 var fs=require('fs-extra');
 
+const THROW_ON_ERR=false;
+
 async function ordenarArchivo(entrada, salida){
     console.log('=========');
     console.log('empezando a ordenar',entrada,salida);
@@ -22,23 +24,40 @@ async function ordenarArchivo(entrada, salida){
         }
         console.log('----------');
         console.log('ok',entrada,salida);
+        return 'ok '+entrada+'=>'+salida;
     }catch(err){
         console.log('----------');
         console.log('ERROR',entrada,salida);
         console.log(err)
+        if(THROW_ON_ERR) throw err;
+        return 'err '+entrada+'=>'+salida+' '+err.message;
     };
 };
 
-ordenarArchivo('/pepe/pepito/pepon/entrada.txt', 'salida_existente.txt');
-ordenarArchivo('entrada.txt', 'salida_existente.txt');
-ordenarArchivo('entrada.txt', 'salida_equivocada.txt');
-fs.unlink('local-salida.txt').catch(function(err){
-    if(err.code!=='ENOENT'){
-        console.log('*************')
-        console.log('ERROR AL BORRAR')
-        console.log(err)
-        throw err;
-    }
-}).then(function(){
-    return ordenarArchivo('entrada.txt', 'local-salida.txt');
-});
+(async function(){
+
+try{
+    var results=[
+          await ordenarArchivo('/pepe/pepito/pepon/entrada.txt', 'salida_existente.txt')
+        , await ordenarArchivo('entrada.txt', 'salida_existente.txt')
+        , await ordenarArchivo('entrada.txt', 'salida_equivocada.txt')
+        , await fs.unlink('local-salida.txt').catch(function(err){
+            if(err.code!=='ENOENT'){
+                console.log('*************')
+                console.log('ERROR AL BORRAR')
+                console.log(err)
+                throw err;
+            }
+        }).then(function(){
+            return ordenarArchivo('entrada.txt', 'local-salida.txt');
+        })
+    ];
+    console.log('/////////////');
+    console.log(results);
+}catch(err){
+    console.log('/////////////!');
+    console.log('ERROR');
+    console.log(err);
+}
+
+})();
--- async/promesas.js
+++ async/promesas.js
@@ -2,6 +2,8 @@
 
 var fs=require('fs-extra');
 
+const THROW_ON_ERR=false;
+
 function ordenarArchivo(entrada, salida){
     console.log('=========');
     console.log('empezando a ordenar',entrada,salida);
@@ -21,23 +23,35 @@ function ordenarArchivo(entrada, salida){
     }).then(function(ok){
         console.log('----------');
         console.log('ok',entrada,salida);
+        return 'ok '+entrada+'=>'+salida;
     },function(err){
         console.log('----------');
         console.log('ERROR',entrada,salida);
         console.log(err)
+        if(THROW_ON_ERR) throw err;
+        return 'err '+entrada+'=>'+salida+' '+err.message;
     });
 };
 
-ordenarArchivo('/pepe/pepito/pepon/entrada.txt', 'salida_existente.txt');
-ordenarArchivo('entrada.txt', 'salida_existente.txt');
-ordenarArchivo('entrada.txt', 'salida_equivocada.txt');
-fs.unlink('local-salida.txt').catch(function(err){
-    if(err.code!=='ENOENT'){
-        console.log('*************')
-        console.log('ERROR AL BORRAR')
-        console.log(err)
-        throw err;
-    }
-}).then(function(){
-    return ordenarArchivo('entrada.txt', 'local-salida.txt');
-});
+Promise.all([
+      ordenarArchivo('/pepe/pepito/pepon/entrada.txt', 'salida_existente.txt')
+    , ordenarArchivo('entrada.txt', 'salida_existente.txt')
+    , ordenarArchivo('entrada.txt', 'salida_equivocada.txt')
+    , fs.unlink('local-salida.txt').catch(function(err){
+        if(err.code!=='ENOENT'){
+            console.log('*************')
+            console.log('ERROR AL BORRAR')
+            console.log(err)
+            throw err;
+        }
+    }).then(function(){
+        return ordenarArchivo('entrada.txt', 'local-salida.txt');
+    })
+]).then(function(results){
+    console.log('/////////////');
+    console.log(results);
+},function(err){
+    console.log('/////////////!');
+    console.log('ERROR');
+    console.log(err);
+},);
