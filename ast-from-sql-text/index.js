a072eaf8c9840e464cafcf022c8d5c46e6675b8d
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-03-21 09:13:40 -0300

    cambio a un sql mas real

--- ast-from-sql-text/index.js
+++ ast-from-sql-text/index.js
@@ -1,21 +1,33 @@
-var sqlExpression = 'SELECT * WHERE foo <> "bar" OR bar LIKE "asfd"';
+// var sqlExpression = "select f(3,a,b>5) AND (c.f + 3 ) :: text || 'hola' is null";
+var sqlExpression = "select f(3,a,b>5) AND (c.f + 3 ) || 'hola' is null";
 
 
 console.log('\n##########AST generado con sqlite-parser ########## ');
-var sqliteParser = require('sqlite-parser');
-var ast = sqliteParser(sqlExpression);
-console.log(JSON.stringify(ast));
+try {
+    var sqliteParser = require('sqlite-parser');
+    var ast = sqliteParser(sqlExpression);
+    console.log(JSON.stringify(ast));
+} catch (e) {
+    console.log('ERROR ', e);
+}
 
 
 console.log('\n##########AST generado con flora-sql-parser ##########');
-var Parser = require('flora-sql-parser').Parser;
-var parser = new Parser();
-var ast = parser.parse(sqlExpression);
-console.log(JSON.stringify(ast));
-
+try {
+    var Parser = require('flora-sql-parser').Parser;
+    var parser = new Parser();
+    var ast = parser.parse(sqlExpression);
+    console.log(JSON.stringify(ast));
+} catch (e) {
+    console.log('ERROR ', e);
+}
 
 console.log('\n##########AST generado con node-sqlparser ########## ');
-var parse = require('node-sqlparser').parse;
-var astObj = parse(sqlExpression);
-console.log(JSON.stringify(astObj));
+try {
+    var parse = require('node-sqlparser').parse;
+    var astObj = parse(sqlExpression);
+    console.log(JSON.stringify(astObj));
+} catch (e) {
+    console.log('ERROR ', e);
+}
 
