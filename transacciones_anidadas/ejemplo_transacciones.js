3d02102ecd6723f9432014a7a2e02dec516ef6f5
Emilio <emilioplatzer@gmail.com>
2018-04-05 14:19:04 -0300

    Transacciones anidadas

--- transacciones_anidadas/ejemplo_transacciones.js
+++ transacciones_anidadas/ejemplo_transacciones.js
@@ -1,17 +1,32 @@
 "use strict";
 
-var pg = require('pg-promise-strict');
+let bestGlobals = require('best-globals');
+let pg = require('pg-promise-strict');
 
-async function probar(conInvalido){
+async ejecutarQuery(client, query){
+    console.log('client', client.numero,'lanzo',query);
+    let result = await client.query(query).execute();
+    console.log('client', client.numero,'listo',query,result);
+    return result;
+}
+
+async function probar(profundidad){
     let client = await pg.connect({
         database: 'test_db',
         user: 'test_user',
         password: 'test_pass',
         port: 5435
     });
-    console.log('connected');
-    var result = await client.query('BEGIN TRANSACTION').execute();
-    console.log(result);
+    client.numero=profundiad;
+    wait ejecutarQuery(client,"BEGIN TRANSACTION");
+    wait ejecutarQuery(client,"INSERT INTO test_trans.table1 id values (99, 'Cliente "+client.numero+"')")
+    console.log('espero para lanzar otro cliente');
+    wait bestGlobals.sleep(1000*profundidad);
+    if(profundidad){
+        probar(profundidad-1);
+    }
+    console.log('espero para reanudar el cliente',client.numero);
+    wait ejecutarQuery(client,"DELETE FROM test_trans.table1 WHERE id=99");
 }
 
-probar();
+probar(1);
