601f7345bda1ea5ed7299f10124066bffbc2f1e8
Emilio <emilioplatzer@gmail.com>
2018-07-27 16:27:58 -0300

    empezando a trabajar con magnitudes

---
+++ postgresql/magnitudes/magnitudes.sql
@@ -0,0 +1,30 @@
+﻿-- magnitudes.sql
+
+drop schema if exists magnitudes cascade;
+create schema magnitudes;
+
+set search_path = magnitudes;
+
+create type magnitud;
+
+CREATE FUNCTION magnitud_in_function(cstring) RETURNS magnitud 
+  LANGUAGE plpgsql AS 
+$BODY$
+begin
+  return null;
+end;
+$BODY$;
+
+CREATE FUNCTION magnitud_out_function(magnitud) RETURNS cstring 
+  LANGUAGE plpgsql AS 
+$BODY$
+begin
+  return null;
+end;
+$BODY$;
+
+create type magnitud(
+    INPUT = magnitud_in_function,
+    OUTPUT = magnitud_out_function
+);
+
