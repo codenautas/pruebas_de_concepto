c8cf51ac3f092a1af41330dfa744b7e6c7c89dbd
Emilio Platzer <emilioplatzer@gmail.com>
2024-12-15 21:11:23 -0300

    Anda (un poco) MUI6 con esm.sh

--- esm/react/src/index.tsx
+++ esm/react/src/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
+// import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
+
 const {date} = require("best-globals");
 
 function NavigationBar() {
     return <div>
         <h1>Hola desde React!</h1>
         <p>hoy es {date.today().toDmy()}</p>
+        <Button>ok</Button>
     </div>;
 }
 
